fc9034b069c89f60d48daecde2aafd44f77264d1
Nikhil Kulkarni <nikhilkulkarni1755@gmail.com>
2024-03-03 04:35:28 -0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Streak-Counter
+Streak Counter
+
+Build Habits by Building a Daily Streak
+
+Work Daily on Goals which you value by measuring your progress with this app.
+
+1) Choose your Daily Goals
+
+2) Create Account/Sign in
+
+3) Create a Goal 
+    a) Take a Picture of what you are working on and assign it.
+    b) mark the days you worked on it. 
+
+4) Track Status
+
+5) Email Updates
+    a) Opt in to get email updates about when your streak expires, weekly stats etc.
+
+    
---
+++ streak-counter/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { CreateComponent } from './create/create.component';
+
+const routes: Routes = [
+  {path:'login', component:LoginComponent},
+  {path:'createAccount', component:CreateComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
