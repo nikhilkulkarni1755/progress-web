84d7fa97e4f48510faa6648b0f0ba0c9c6b5c2f2
root <root@MSI>
2025-05-17 19:09:52 -0700

    work on scaling elements

--- progress-web-new/progress-web/src/App.tsx
+++ progress-web-new/progress-web/src/App.tsx
@@ -2,27 +2,35 @@ import './App.css'
 
 function App() {
   return (
-    <div className="w-screen flex flex-col min-h-screen">
+    <div className="w-screen flex flex-col min-h-screen border-black bg-gray-500">  
       {/* Header */}
-      <header className="w-full flex justify-between items-center py-20 px-6 pr-75 bg-[#dff6ff] text-black shadow rounded-r-[2vw]"
+      <header className="w-full flex justify-between items-center py-24 px-6 pr-24 bg-[#dff6ff] text-black shadow rounded-r-[2vw] max-w-full"
       style={{
         background: "linear-gradient(90deg,rgba(223, 246, 255, 1) 0%, rgba(255, 223, 246, 1) 50%, rgba(246, 255, 223, 1) 100%)",
       }}>
-        <div className="flex items-center space-x-5">
-          <h1 className="text-3xl font-bold">Build better habits with 
-            <span className="text-[#4682B4] italic ml-3">The Progress App</span>
-          </h1>
+        <div className="flex flex-col items-start max-w-full overflow-hidden flex-grow max-w-[50%]">
+          <p className="text-4xl font-semibold text-gray-700 leading-tight">
+            Build better habits with
+          </p>
+          <p className="text-8xl font-bold italic text-[#4682B4] leading-tight">
+            The Progress App
+          </p>
         </div>
 
-        <img
-              src="src\assets\phone.png"
-              alt="App Logo"
-              className="w-[200px] h-auto pr-*"
-            />
+
+        {/* text-[#4682B4] */}
+        <div className='flex items-center max-w-[50%]'>
+          <img
+            src="src\assets\phone.png"
+            alt="App Logo"
+            className="w-[200px] h-auto mr-12"
+          />
+        </div>
+
       </header>
 
       {/* Body */}
-      <main className="w-full grid grid-cols-3 gap-6 px-6 py-12 bg-white">
+      <main className="w-full px-[10%] grid grid-cols-3 gap-6 px-6 py-12 bg-green">
         {/* Review Section */}
         <div className="bg-gray-100 p-6 rounded shadow min-h-[400px]">
           <h2 className="text-2xl font-semibold mb-4">Review</h2>
